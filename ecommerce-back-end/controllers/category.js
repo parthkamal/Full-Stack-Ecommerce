a29f66c5886a6c4720bf79d4e495b4ed72302f7a
Parth Kamal <kamalparth40@gmail.com>
2022-08-11 17:46:07 +0530

    middleware to all the categories in the hierarchical form

--- ecommerce-back-end/controllers/category.js
+++ ecommerce-back-end/controllers/category.js
@@ -26,14 +26,31 @@ exports.createCatergoryController = (req, res) => {
     })
 }
 
+function createCategories(categories,parentId=null){
+    //middleware to all the categories in the hirarchical form
+    const categoryList=[];
+    let category;
+    if(parentId==null)category=categories.filter(cat=>cat.parentId ===null );
+    else category=categories.filter(cat=>cat.parentId==parentId);
 
+    for(let cat of category){
+        categoryList.push({
+            _id:cat._id,
+            name: cat.name,
+            slug:cat.slug,
+            children:createCategories(categories,cat._id)
+        });
+    }
+    console.log(categoryList)
+    return categoryList;
+}
 
 //get category controller can be accessed from any one
 exports.getCategoryController = (req, res) => {
     //extracting all the categories from the database and returning to the user
-    Category.find((error, categoryList) => {
-        if (error) return res.status(400).json(error)
-        return res.status(200).json(categoryList);
+    Category.find((error, categories) => {
+        if (error) return res.status(400).json(error);
+        if(categories)return res.status(200).json({categories:createCategories(categories)})
     })
 }
 
