89fb4e356422c6d40eb3574830e54524df74d898
Parth Kamal <kamalparth40@gmail.com>
2022-08-11 14:51:18 +0530

    added the category controller

--- ecommerce-back-end/controllers/category.js
+++ ecommerce-back-end/controllers/category.js
@@ -1,6 +1,36 @@
-const Category=require('../models/category')
+const Category = require('../models/category')
+const slugify = require('slugify')
 
-exports.createCatergoryController=(req,res)=>{
+exports.createCatergoryController = (req, res) => {
+    //destructure the attributes from the req object
+    const { name, parentId } = req.body;
+    if (!parentId) {
+        //it means the parent category is going to be created
+        const categoryObject = { name, slug: slugify(name) }
+        const category = new Category(categoryObject);
+        category.save((error, category) => {
+            if (error) return res.status(400).json(error)
+            return res.status(200).json(category)
+        })
+    };
+    //it means a subcategory is being created
+    const categoryObject = {
+        name, parentId, slug: slugify(name)
+    }
+    const category = new Category(categoryObject);
+    //saving category to the database
+    category.save((error, category) => {
+        if (error) return res.status(400).json(error);
+        return res.status(200).json(category)
+    })
+}
 
+
+exports.getCategoryController = (req, res) => {
+    //extracting all the categories from the database and returning to the user
+    Category.find((error, categoryList) => {
+        if (error) return res.status(400).json(error)
+        return res.status(200).json(categoryList);
+    })
 }
 
--- ecommerce-back-end/routes/category.js
+++ ecommerce-back-end/routes/category.js
@@ -1,20 +1,13 @@
 //importing the express module
-const express=require('express')
+const express=require('express');
+const { createCatergoryController, getCategoryController } = require('../controllers/category');
 const router=express.Router();
 
 
 //get and post routes for the category model which will be ulimately handled by the model controller
-router.post('/category/create',(req,res)=>{
-    res.status(200).json({
-        message:"create category route"
-    })
-})
+router.post('/category/create',createCatergoryController)
 
-router.get('/category/get',(req,res)=>{
-    res.status(200).json({
-        message:"get category route"
-    })
-})
+router.get('/category/get',getCategoryController)
 
 //exporting the router so that it can be used by the server from index.server.js
 module.exports=router
