76cfcb0b352b049cecacb480a6084426930aa388
Parth Kamal <kamalparth40@gmail.com>
2022-08-11 14:02:22 +0530

    added category model

---
+++ ecommerce-back-end/models/category.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose')
+//importing the mongoose model library
+
+const categorySchema= new mongoose.Schema({
+    name:{
+        required:true,
+        type:String,
+        trim:true,
+    },
+    slug:{
+        required:true,
+        type:String,
+        unique:true,
+        type:String
+    },
+    parentId:{
+        type:String,
+        default:null
+    }
+
+},{timestamps:true})
+
+module.exports=mongoose.model('Category',categorySchema)
+
