94255e0e2a0f3e5355781a9080d5f08fa1453aeb
Parth Kamal <kamalparth40@gmail.com>
2022-08-17 22:13:49 +0530

    added the admin redux store

--- ecommerce-back-end/controllers/cart.js
+++ ecommerce-back-end/controllers/cart.js
@@ -1,37 +1,37 @@
-const Cart =require('../models/cart')
+const Cart = require('../models/cart')
 
 
 //controller for adding items to cart
-exports.addToCartController = (req,res)=>{
-    
-    Cart.findOne({user:req.user._id}).exec((error,cart)=>{
-        if(error)return res.status(400).json({error})
-        if(cart){
+exports.addToCartController = (req, res) => {
+
+    Cart.findOne({ user: req.user._id }).exec((error, cart) => {
+        if (error) return res.status(400).json({ error })
+        if (cart) {
             //if the cart already exists then we only need to update it 
             const product = req.body.cartItems.product
-            const item =cart.cartItems.find(c=> c.product==product)
-            if(item){
-                Cart.findOneAndUpdate({"user":req.user._id,"cartItems.product":product},{
-                    "$set":{
-                        "cartItems":{
+            const item = cart.cartItems.find(c => c.product == product)
+            if (item) {
+                Cart.findOneAndUpdate({ "user": req.user._id, "cartItems.product": product }, {
+                    "$set": {
+                        "cartItems": {
                             ...req.body.cartItems,
-                            quantity:item.quantity + req.body.cartItems.quantity
+                            quantity: item.quantity + req.body.cartItems.quantity
                         }
                     }
-                }).exec((error,cart)=>{
-                    if(error)return res.status(400).json({error})
-                    if(cart)return res.status(200).json({cart})
+                }).exec((error, cart) => {
+                    if (error) return res.status(400).json({ error })
+                    if (cart) return res.status(200).json({ cart })
                 })
             }
-        }else{
+        } else {
             //create a new cart
             const cart = new Cart({
-                user:req.user._id,
-                cartItems:req.body.cartItems
+                user: req.user._id,
+                cartItems: req.body.cartItems
             })
-            cart.save((error,cart)=>{
-                if(error)return res.status(400).json({error})
-                if(cart)return res.status(200).json({cart})
+            cart.save((error, cart) => {
+                if (error) return res.status(400).json({ error })
+                if (cart) return res.status(200).json({ cart })
             })
         }
     })
